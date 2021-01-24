30b600f6ebfb29cb685e53b960d7bf521943cb9e
papapepekk <77702436+papapepekk@users.noreply.github.com>
2021-01-24 13:09:25 -0300

    Add files via upload

---
+++ src/toinmenu.js
@@ -0,0 +1,12 @@
+const toinmenu = (prefix, pushname) => {
+    return `◪ *Comandos do bot da ♛•ᏞᏟ'ร•♛*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.toinmenu = toinmenu
